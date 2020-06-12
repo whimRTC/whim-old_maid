0f0b6aa6853802118a550f555f108fa05f3db3e3
Odagawa <xtctl.1234@gmail.com>
2020-06-12 23:08:04 +0900

    set global font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import whimClientVue from "whim-client-vue";
 import VuePlayingCard from "vue-playing-card";
 import "whim-client-vue/dist/whim-client-vue.css";
+import "./assets/main.scss";
 
 Vue.config.productionTip = false;
 Vue.use(whimClientVue);
