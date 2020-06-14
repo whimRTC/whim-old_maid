764c21137caf39258dedb4b44f43986eac8cdb5d
Odagawa <xtctl.1234@gmail.com>
2020-06-14 16:21:08 +0900

    fix the place of retry button

--- src/components/main/Index.vue
+++ src/components/main/Index.vue
@@ -80,8 +80,8 @@ export default {
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
     0 3px 1px -2px rgba(0, 0, 0, 0.2);
   transition: 0.3s ease-out;
-  position: relative;
-  top: 50vh;
+  position: absolute;
+  top: 80vh;
   left: 50vw;
   transform: translate(-50%, -50%);
   width: 100px;
