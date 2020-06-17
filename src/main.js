cb4a35b04a42e69f0c398f28901051edc81c710f
Odagawa <xtctl.1234@gmail.com>
2020-06-17 20:48:22 +0900

    refactor

--- src/main.js
+++ src/main.js
@@ -9,12 +9,6 @@ Vue.config.productionTip = false;
 Vue.use(whimClientVue);
 Vue.use(VuePlayingCard);
 
-Vue.prototype.$sendSE = () => {
-  Vue.prototype.$whim.assignState({
-    sound: true
-  });
-};
-
 new Vue({
   render: h => h(App)
 }).$mount("#app");
